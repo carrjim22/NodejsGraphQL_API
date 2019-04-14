c06537e0863a6a040fb8032a3e115510cc76ce50
carrjim <carrjim22@gmail.com>
2019-04-14 13:17:02 -0500

    Db modificado

--- src/db.js
+++ src/db.js
@@ -4,5 +4,5 @@ export const connect = async () =>{
    await mongoose.connect(process.env.DATABASE_URI, {
         useNewUrlParser: true
     })
-    console.log('Database is conected')
+    console.log('MongoDB is conected')
 }
